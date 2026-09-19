81bd0bda8d7f775310265e36edac3a223f7183bf
almen <almenling18@gmail.com>
2026-09-19 16:02:03 +0800

    Add house sorting exercise

---
+++ CS50P_Lecture_1.py
@@ -0,0 +1,9 @@
+x = int (input("What's your favorite number? "))
+y = int (input("Let's enter another number? "))
+
+if x < y:
+    print(f"{x} is less than {y}.")
+if x > y:
+    print(f"{x} is greater than {y}.")
+if x == y:
+    print(f"{x} is equal to {y}.")
---
+++ CS50P_Lecture_1_house.py
@@ -0,0 +1,11 @@
+from unittest import case
+
+name = input("Enter your name: ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
